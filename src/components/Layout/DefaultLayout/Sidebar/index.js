9cf1ea4b037f8a226155adc3047b17324dc50238
huukhuong-vn <dinhhuukhuong1995@gmail.com>
2024-10-19 05:19:59 +0700

    Dựng khung layout dự án

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,9 @@
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return <h2 className={cx('wrapper')}>Sidebar</h2>;
 }
 
 export default Sidebar;
